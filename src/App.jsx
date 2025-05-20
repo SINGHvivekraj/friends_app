64a1cca352860af0d6a480058a9cc1a49c7427e8
viveksingh <viveks98765@gmail.com>
2025-05-20 18:38:04 +0530

    added realtime friends count functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,10 @@ function App() {
   return (
     <>
      <div className="container w-full h-screen bg-zinc-700">
-
+      <div className="navbar">
+        <h1 className="text-xl font-bold text-white p-4">Your Friend count : {
+          data.filter(item=>item.isFriend).length} </h1>
+      </div>
       <div className="cards  flex justify-center wrap p-4 gap-3">
         {data.map((ele,idx)=>{
           return <Card key={idx} data={ele} index={idx} changeFriendStatus={changeFriendStatus}/>
